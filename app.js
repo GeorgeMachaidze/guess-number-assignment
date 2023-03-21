a2fedaadd2c3da4b288ff3b5a898da6fdb73f8a6
Wakana <wakaichi@gmail.com>
2023-03-21 15:22:37 +0400

    add app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+let guess = document.getElementById("guess");
+let checkButton = document.getElementById("checkButton");
+let tryButton = document.getElementById("try");
+let randomInteger = Math.floor(Math.random() * 100);
+let number = document.querySelector(".number");
+let p = document.querySelector(".message");
+let pScore = document.querySelector(".score");
+let pHighScore = document.querySelector(".highscore");
+let score = 0;
+let highscore = 100;
+console.log(randomInteger);
+
+
+function tryAgain(){
+    score = 0;
+    pScore.innerHTML = "0"
+    randomInteger = Math.floor(Math.random() * 100);
+    console.log(randomInteger);
+    document.body.style.backgroundColor = "#222";
+    number.innerHTML = "?";
+}
+function checkUserInput(){
+    if(guess.value > 100 || guess.value < 1){
+        alert("Please chose number between 1 and 100")
+    }
+}
+function checkInputHightOrLow(){
+    if(guess.value > randomInteger){
+        score++;
+        p.innerHTML = "  To High!"
+        pScore.innerHTML = score;
+    }if(guess.value < randomInteger){
+        score++;
+        p.innerHTML = "To Low!"
+        pScore.innerHTML = score;
+    }if(guess.value == randomInteger){
+        score++;
+        pScore.innerHTML = score;
+        p.innerHTML = "Correct number!"
+        document.body.style.backgroundColor = "#60b347";
+        checkHighScore();
+        number.innerHTML = randomInteger;
+    }
+}
+function checkHighScore(){
+    if(score < highscore){
+        pHighScore.innerHTML = score;
+    }
+}
+
+checkButton.addEventListener("click", function(){
+    checkUserInput();
+    checkInputHightOrLow();
+})
+tryButton.addEventListener("click", function(){
+    tryAgain();
+    
+})
